ad6a107837bd1dab6a3febc135f44cec955dcc57
therealthaps <abthapa@knox.edu>
2021-04-06 15:01:09 -0500

    Added backend to digital ocean and editied .env.docker with the Floating ip

---
+++ client/deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+# build
+npm run build-docker
+# navigate into the build output directory
+cd dist
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:therealthaps/rev.git master:gh-pages
+cd -
