b38de14b7e1e69af5a1852e7737039a33d8f7a3c
therealthaps <abthapa@knox.edu>
2021-04-07 12:24:59 -0500

    First commit on Development

--- client/deploy.sh
+++ client/deploy.sh
@@ -2,7 +2,7 @@
 # abort on errors
 set -e
 # build
-npm run build-docker
+npm run build
 # navigate into the build output directory
 cd dist
 # if you are deploying to a custom domain
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:asaadielhassan/revue.git master:gh-pages
+git push -f git@github.com:therealthaps/revue.git master:gh-pages
 cd -
